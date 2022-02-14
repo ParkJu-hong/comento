fb9c7b18482dec18af8945c6c4e170bb4ebb906c
bejeju <bejejupark@gmail.com>
2022-02-14 20:45:11 +0900

    22.02.14

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -12,6 +12,7 @@ function Calender() {
   useEffect(()=>{
     console.log('오늘 => ', new Date().getFullYear(), '/', new Date().getMonth(), '/',new Date().getDate());
   },[])
+  
 
 
   const modifiersStyles = {
@@ -50,16 +51,71 @@ function Calender() {
       <DayPicker onDayClick={(day) => {
         console.log(day.getMonth() + 1);
         console.log(day.getDate());
-
         console.log(day.getFullYear);
       }}
         selectedDays={undefined}
         modifiersStyles={modifiersStyles}
-
+        renderDay={renderDay}
       />
     </div>
   );
 }
+
+function renderDay(data: any){
+  return (
+  <div style={{ 
+    height: "100%",
+    width: "100%",
+    position: 'relative'}}>
+      <div style={{
+        position: 'absolute',
+        color: 'black',
+        bottom: 0,
+        right: 0,
+        fontSize: 30,
+      }}>
+
+      </div>
+  </div>)
+}
+
+// function renderDay(day:any):any {
+//   const birthdays = {
+//     3: ['Mirko', 'Gianni'],
+//     8: ['Elena'],
+//     9: ['Irene'],
+//     12: ['Paolo', 'Giorgia'],
+//     18: ['Claudia'],
+//     22: ['Maria', 'Luigi'],
+//     25: ['Simone'],
+//     26: ['Marta'],
+//   };
+//   const date = day.getDate();
+//   const dateStyle = {
+//     position: 'absolute',
+//     color: 'lightgray',
+//     bottom: 0,
+//     right: 0,
+//     fontSize: 20,
+//   };
+//   const birthdayStyle = { fontSize: '0.8em', textAlign: 'left' };
+//   const cellStyle = {
+//     height: 50,
+//     width: 60,
+//     position: 'relative',
+//   };
+//   return (
+//     <div style={cellStyle}>
+//       <div style={dateStyle}>{date}</div>
+//       {birthdays[date] &&
+//         birthdays[date].map((name, i) => (
+//           <div key={i} style={birthdayStyle}>
+//             🎁 {name}
+//           </div>
+//         ))}
+//     </div>
+//   );
+// }
 
 export default Calender;
 
